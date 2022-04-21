27d3a6c8ec1e09a4033229c66721bf6c864a92d6
danbai <danbai@88.com>
2022-04-21 11:47:10 +0800

    1

--- background.js
+++ background.js
@@ -1,19 +1,20 @@
-import fetch from "./fetch.min.js"
-chrome.runtime.onMessage.addListener(  function (request, sender, sendResponse) {
-  switch(request.type){
-    case "postText":{
-     someAsyncFunction(request.data);
-        return true;
+import fetch from "./fetch.min.js";
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  switch (request.type) {
+    case "postText": {
+      postDataFunc(request.data);
+      break;
     }
   }
 });
 
-function someAsyncFunction(data){
-       fetch.fetch('http://127.0.0.1:49492/trace/add', {
-        method: 'POST',
-         headers: {
-          'Content-Type': 'application/json'
-         },
-         body: data
-      });
+function postDataFunc(data) {
+  fetch.fetch("http://127.0.0.1:49492/trace/add", {
+    mode: "no-cors",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: data,
+  });
 }
--- content-script.js
+++ content-script.js
@@ -19,87 +19,90 @@ function replace(str, substr, newstr) {
  * @param f 小数位精确度，默认2位
  * @returns {string|number|*} 百分数前的数值，最大100. 比如 ：90.32
  */
- function similar(s, t, f) {
+function similar(s, t, f) {
   if (!s || !t) {
-    return 0
+    return 0;
   }
-  if(s === t){
+  if (s === t) {
     return 100;
   }
-  var l = s.length > t.length ? s.length : t.length
-  var n = s.length
-  var m = t.length
-  var d = []
-  f = f || 2
+  var l = s.length > t.length ? s.length : t.length;
+  var n = s.length;
+  var m = t.length;
+  var d = [];
+  f = f || 2;
   var min = function (a, b, c) {
-    return a < b ? (a < c ? a : c) : (b < c ? b : c)
-  }
-  var i, j, si, tj, cost
-  if (n === 0) return m
-  if (m === 0) return n
+    return a < b ? (a < c ? a : c) : b < c ? b : c;
+  };
+  var i, j, si, tj, cost;
+  if (n === 0) return m;
+  if (m === 0) return n;
   for (i = 0; i <= n; i++) {
-    d[i] = []
-    d[i][0] = i
+    d[i] = [];
+    d[i][0] = i;
   }
   for (j = 0; j <= m; j++) {
-    d[0][j] = j
+    d[0][j] = j;
   }
   for (i = 1; i <= n; i++) {
-    si = s.charAt(i - 1)
+    si = s.charAt(i - 1);
     for (j = 1; j <= m; j++) {
-      tj = t.charAt(j - 1)
+      tj = t.charAt(j - 1);
       if (si === tj) {
-        cost = 0
+        cost = 0;
       } else {
-        cost = 1
+        cost = 1;
       }
-      d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+      d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost);
     }
   }
-  let res = (1 - d[n][m] / l) *100
-  return res.toFixed(f)
+  let res = (1 - d[n][m] / l) * 100;
+  return res.toFixed(f);
 }
 //获取页面内容
-function getText(){
+function getText() {
   var contentStr = document.documentElement.innerText;
   contentStr = replace(contentStr, "\n", "");
   contentStr = replace(contentStr, "\t", "");
   contentStr = replace(contentStr, "\r", "");
   contentStr = replace(contentStr, " ", "");
   contentStr = replace(contentStr, " ", "");
-  return contentStr
+  return contentStr;
 }
 //提交页面数据
-function postText(text){
-  oldText=text;
+function postText(text) {
+  oldText = text;
   if (text.length > 0) {
-    chrome.runtime.sendMessage({type:"postText",data: JSON.stringify({
-      title: document.title,
-      content: text,
-      url: document.URL,
-    }) });
+    chrome.runtime.sendMessage({
+      type: "postText",
+      data: JSON.stringify({
+        title: document.title,
+        content: text,
+        url: document.URL,
+      }),
+    });
   }
 }
-function myChange(){
-  text=getText();
-  if (similar(oldText,text)<80){
+function myChange() {
+  text = getText();
+  if (similar(oldText, text) < 80) {
     postText(text);
   }
 }
 //监听url变化
-function ListeningUrlChanges(){
-  var bodyList = document.querySelector("body")
-  var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-          if (oldHref != document.location.href) {
-              oldHref = document.location.href;
-              myChange();
-          }
-      });
+function ListeningUrlChanges() {
+  var bodyList = document.querySelector("body");
+  var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (oldHref != document.location.href) {
+        oldHref = document.location.href;
+        myChange();
+      }
+    });
   });
   var config = {
-      childList: true,
-      subtree: true
+    childList: true,
+    subtree: true,
   };
   observer.observe(bodyList, config);
 }
