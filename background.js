e08cd831d8fd12d2b26b305dc61f253673448ad2
Bibooo <2573018577@qq.com>
2022-04-21 11:22:28 +0800

    消息在响应之前关闭

--- background.js
+++ background.js
@@ -1,15 +1,19 @@
 import fetch from "./fetch.min.js"
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(  function (request, sender, sendResponse) {
   switch(request.type){
-    case "postText":
-       fetch.fetch('http://127.0.0.1:49492/trace/add', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: request.data
-      });
-      break
+    case "postText":{
+     someAsyncFunction(request.data);
+        return true;
+    }
   }
 });
 
+function someAsyncFunction(data){
+       fetch.fetch('http://127.0.0.1:49492/trace/add', {
+        method: 'POST',
+         headers: {
+          'Content-Type': 'application/json'
+         },
+         body: data
+      });
+}
